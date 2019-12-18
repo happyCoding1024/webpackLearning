69aaa4e5cf391cee9236e1bba06d59ddebca21da
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-18 11:27:16 +0800

    webpack environment install

---
+++ webpack-demo/index.js
@@ -0,0 +1,9 @@
+import Header from './header'
+import Sidebar from './sidebar'
+import Content from './content'
+
+var dom = document.getElementById('root')
+
+new Header();
+new Sidebar();
+new Content();
