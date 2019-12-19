4ea950a3fd5584cbf8e7c02c4a1c37151a9a07e8
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-19 15:41:58 +0800

    use html-webpack and clean-webpack plugin

--- webpack-demo/lesson/webpack.config.js
+++ webpack-demo/lesson/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   // 打包的模式,，默认的模式是 production
@@ -27,11 +29,19 @@ module.exports = {
       use: {
         loader: 'file-loader',
       }
-    },]
+    }]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets:true, //自动删除未被使用的webpack资源
+    })
+  ],
   // 打包好文件的信息
   output: {
-    filename: 'bundle.js',
+    filename: 'dist1.js',
     // 不能直接写相对路径，必须借助node中的path模块
     path: path.resolve(__dirname, 'dist'),
   }
